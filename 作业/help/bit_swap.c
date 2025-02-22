4cc2c4b68f1b4315985a999b05434915c3d8593e
Taneck <2469980593@qq.com>
2025-02-22 22:50:00 +0800

    no feat

--- 作业/help/bit_swap.c
+++ 作业/help/bit_swap.c
@@ -1,1 +1,10 @@
-\n
+uint64_t bit_swap(uint64_t value){
+    uint64_t even_bits = value & 0xaaaaaaaaaaaaaaaa;
+    uint64_t odd_bits = value & 0x5555555555555555;
+
+    even_bits >>= 1;
+    odd_bits <<= 1;
+
+
+    return(even_bits|odd_bits);
+}
